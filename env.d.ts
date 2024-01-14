7fd90f140a741e934f291675b496eb724ac37f66
Tom Plant <tom@tplant.com.au>
2024-01-14 10:11:47 +0000

    feat: optionally load monitors from Cloudflare proxied DNS records
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,8 @@ declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
 			UPTIMEFLARE_STATE: KVNamespace;
+			CLOUDFLARE_ZONE_ID?: string;
+			CLOUDFLARE_API_TOKEN?: string;
 		}
 	}
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,8 @@ import MonitorList from '@/components/MonitorList'
 import { Center, Divider, Text } from '@mantine/core'
 import MonitorDetail from '@/components/MonitorDetail'
 
+import { dnsRecords } from 'cloudflare-client'
+
 export const runtime = 'experimental-edge'
 const inter = Inter({ subsets: ['latin'] })
 
@@ -93,15 +95,35 @@ export default function Home({
 }
 
 export async function getServerSideProps() {
-  const { UPTIMEFLARE_STATE } = process.env as unknown as {
-    UPTIMEFLARE_STATE: KVNamespace
+  const { UPTIMEFLARE_STATE, CLOUDFLARE_ZONE_ID, CLOUDFLARE_API_TOKEN } = process.env as unknown as {
+    UPTIMEFLARE_STATE: KVNamespace,
+    CLOUDFLARE_ZONE_ID: string,
+    CLOUDFLARE_API_TOKEN: string
   }
 
   // Read state as string from KV, to avoid hitting server-side cpu time limit
   const state = (await UPTIMEFLARE_STATE?.get('state')) as unknown as MonitorState
 
+  // load monitors from Cloudflare proxied DNS records. official API only supports node
+  let cfMonitors: { id: string, name: string, tooltip: string }[] = []
+  try {
+    const cf = dnsRecords({
+      zoneId: CLOUDFLARE_ZONE_ID,
+      accessToken: CLOUDFLARE_API_TOKEN
+    })
+    const records = await cf.find({ proxied: true }).all()
+    
+    cfMonitors = records.map(monitor => ({
+      id: monitor.name,
+      name: monitor.name,
+      tooltip: `https://${monitor.name}/`,
+    }))
+  } catch(err) {
+    console.log(`Skipping Cloudflare auto-discovery: ${err}`)
+  }
+
   // Only present these values to client
-  const monitors = workerConfig.monitors.map((monitor) => {
+  let monitors = workerConfig.monitors.map(monitor => {
     return {
       id: monitor.id,
       name: monitor.name,
@@ -110,7 +132,11 @@ export async function getServerSideProps() {
       // @ts-ignore
       statusPageLink: monitor?.statusPageLink
     }
-  })
+  }).concat(cfMonitors)
+  monitors = monitors.reduce((acc, curr) => {
+    if (!acc.find(item => item.id === curr.id)) acc.push(curr)
+    return acc
+  }, [] as any[])
 
   return { props: { state, monitors } }
 }
